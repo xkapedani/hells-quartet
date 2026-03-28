fe603f954de0f53d4f1fb6bc348cdf035b746514
xkapedani <73874899+xkapedani@users.noreply.github.com>
2026-03-28 11:52:46 +0100

    Add buttons

--- src/Homepage.js
+++ src/Homepage.js
@@ -1,24 +1,30 @@
 // four buttons on the homepage, each with an icon and a label
-import React from 'react';
-import './Homepage.css';
+import React from "react";
+import "./Homepage.css";
 
+const publicEnv = process.env.PUBLIC_URL;
+
+// add
 function Homepage() {
-  return (
-    <div className="homepage">
-      <button className="homepage-button">
-        <span>drummer</span>
-      </button>
-      <button className="homepage-button">
-        <span>guitarist</span>
-      </button>
-      <button className="homepage-button">
-        <span>bassist</span>
-      </button>
-      <button className="homepage-button">
-        <span>vocalist</span>
-      </button>
-    </div>
-  );
+    return (
+        <div className="homepage">
+            <button className="homepage-button">
+                <img src="${publicEnv}/drummer.svg}" alt="Drummer" />
+            </button>
+            <button className="homepage-button">
+                <img
+                    src="${publicEnv}/acoustic-guitar-musician.svg}"
+                    alt="Drummer"
+                />
+            </button>
+            <button className="homepage-button">
+                <span>bassist</span>
+            </button>
+            <button className="homepage-button">
+                <span>vocalist</span>
+            </button>
+        </div>
+    );
 }
 
 export default Homepage;
