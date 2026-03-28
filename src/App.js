ffce642dc3f8f13aa1e36777dded810340705e49
xkapedani <73874899+xkapedani@users.noreply.github.com>
2026-03-28 16:58:32 +0100

    Added basic homepage

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
-import './App.css';
-import { Route, Routes } from 'react-router-dom';
-import Homepage from './Homepage.js';
-import Trio from './puzzles/Trio.js';
-import Drums from './puzzles/Drums.js';
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Homepage from "./Homepage.js";
+import Trio from "./puzzles/Trio.js";
+import Drums from "./puzzles/Drums.js";
 
 function App() {
-  return (
-    <div className="App">
-      <Routes>
-        <Route path="/hells-quartet" element={<Homepage />} />
-        <Route path="/hells-quartet/trio" element={<Trio />} />
-        <Route path="/hells-quartet/drums" element={<Drums />} />
-      </Routes>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Routes>
+                <Route path="/hells-quartet" element={<Homepage />} />
+                <Route path="/hells-quartet/trio" element={<Trio />} />
+                <Route path="/hells-quartet/drums" element={<Drums />} />
+                {/* Catch-all redirects to homepage */}
+                <Route path="*" element={<Homepage />} />
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
